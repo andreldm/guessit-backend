1996c3b3e34677506186fa3f0970be2664450dba
Andre Miranda <andre42m@gmail.com>
2016-09-04 14:34:40 -0300

    Fix cards_test.js

--- tests/cards_test.js
+++ tests/cards_test.js
@@ -1,11 +1,12 @@
-let cards = require('../cards.js');
+let CardManager = require('../card-manager.js');
+let cardManager = new CardManager();
 
-let deck = cards.getDeck();
+let deck = cardManager.getDeck();
 console.log(deck);
 
 for (let i = 0; i < 50; i++) {
   let card = deck[0];
   deck.splice(0, 1);
-  deck.push(cards.exchangeCard(card));
+  deck.push(cardManager.exchangeCard(card));
   console.log(deck);
 }
