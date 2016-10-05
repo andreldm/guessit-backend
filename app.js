4e287e9545ea6a34f6ca57e0791ac8026790321a
Gean Carlo Peixoto <geancpeixoto@gmail.com>
2016-10-04 21:43:05 -0300

    app.js identado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let http = require('http');
 let socketio = require('socket.io');
 
 let partyManager = require("./dist/party/party-manager");
-let playerStore  = require("./dist/player/player-store");
+let playerStore = require("./dist/player/player-store");
 
 let app = express();
 let server = http.Server(app);
@@ -12,51 +12,54 @@ let io = socketio(server);
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 
-app.get('/reset',(req, res) => {
+app.get('/reset', (req, res) => {
   console.log("Resetting...");
   partyManager.default.reset();
   res.send("reset done!");
 });
 
-partyManager.default.onGameOver.subscribe(winner=>{
+partyManager.default.onGameOver.subscribe(winner => {
   io.emit('gameover', winner);
 });
-partyManager.default.onUpdate.subscribe(()=>{
+partyManager.default.onUpdate.subscribe(() => {
   console.log('update all players!');
   io.emit('update-all', playerStore.default.get());
 });
-partyManager.default.onCardsBet.subscribe(cards=>{
+partyManager.default.onCardsBet.subscribe(cards => {
   console.log('send cards to bet!');
   io.emit('cards-bet', cards);
 });
-partyManager.default.onBetsReveled.subscribe(bets=>{
+partyManager.default.onBetsReveled.subscribe(bets => {
   console.log('reveled all bets!');
   io.emit('bets-reveled', bets);
 });
 
-io.on('connection',(socket)=>{  
-  socket.on('join',(playerId,playerName)=>{
+io.on('connection', (socket) => {
+  socket.on('join', (playerId, playerName) => {
     partyManager.default.join({
-      id:playerId
-      ,name:playerName
+      id: playerId,
+      name: playerName
     });
   });
   //socket.on('disconnect', function () { playerManager.handleDisconnect(io, socket); });
-  //socket.on('reconnect-player',(playerId,playerName)=>{ playerManager.handleReconnect(io, socket, playerId, playerName); }); 
-  socket.on('pick-card',(playerId,cardId)=>{ 
-    partyManager.default.pickCard(playerId,cardId); 
+  //socket.on('reconnect-player',(playerId,playerName)=>{ playerManager.handleReconnect(io, socket, playerId, playerName); });
+  socket.on('pick-card', (playerId, cardId) => {
+    partyManager.default.pickCard(playerId, cardId);
   });
-  socket.on('pick-bet',(playerId,cardId)=>{ 
-    partyManager.default.betCard(playerId,cardId); 
+  socket.on('pick-bet', (playerId, cardId) => {
+    partyManager.default.betCard(playerId, cardId);
   });
-  socket.on('discard-card',(playerId,cardId)=>{ 
-    partyManager.default.discardCard(playerId,cardId); 
+  socket.on('discard-card', (playerId, cardId) => {
+    partyManager.default.discardCard(playerId, cardId);
   });
-  socket.on('rename-player',(playerId,newName)=>{ 
-    partyManager.default.renamePlayer({id:playerId,name:newName}); 
+  socket.on('rename-player', (playerId, newName) => {
+    partyManager.default.renamePlayer({
+      id: playerId,
+      name: newName
+    });
   });
 });
 
-server.listen(app.get('port'),()=>{
+server.listen(app.get('port'), () => {
   console.log(`listening on :${app.get('port')}`);
 });
